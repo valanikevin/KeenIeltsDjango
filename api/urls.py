829615b2dc0564883d3e81d9ad0543fdd4627839
Kevin Valani <valanikevin@gmail.com>
2023-07-19 18:55:02 -0600

    added test notes app

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', api_views.getRoutes),
+    path('notes/', api_views.getNotes),
     path('token/', api_views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- custom_user/models.py
+++ custom_user/models.py
@@ -1,5 +1,11 @@
 from django_use_email_as_username.models import BaseUser, BaseUserManager
+from django.db import models
 
 
 class User(BaseUser):
     objects = BaseUserManager()
+
+
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
