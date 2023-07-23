92e75c310b0a00bf62d76f1edbb3a274c11c94aa
Kevin Valani <valanikevin@gmail.com>
2023-07-23 13:36:00 -0600

    added tutor model

--- coachinginstitute/admin.py
+++ coachinginstitute/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from coachinginstitute.models import CoachingInstitute
 
 # Register your models here.
+
+
+class CoachingInstituteAdmin(admin.ModelAdmin):
+    search_fields = ['name',]
+
+
+admin.site.register(CoachingInstitute, CoachingInstituteAdmin)
