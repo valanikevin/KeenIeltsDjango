156d36cabc2ded846e0f93bc44427a4384b6d26f
Kevin Valani <valanikevin@gmail.com>
2023-07-23 13:52:36 -0600

    changed the database to postgres

--- KeenIeltsDjango/settings.py
+++ KeenIeltsDjango/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = 'KeenIeltsDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'keenielts',
+        'USER': 'keenielts',
+        'PASSWORD': 'keen1289',
+        'HOST': 'localhost'
     }
 }
 
--- coachinginstitute/admin.py
+++ coachinginstitute/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from coachinginstitute.models import CoachingInstitute
+from coachinginstitute.models import CoachingInstitute, Tutor
 
 # Register your models here.
 
 
+class TutorInline(admin.StackedInline):
+    model = Tutor
+
+
 class CoachingInstituteAdmin(admin.ModelAdmin):
     search_fields = ['name',]
+    inlines = [TutorInline,]
 
 
 admin.site.register(CoachingInstitute, CoachingInstituteAdmin)
