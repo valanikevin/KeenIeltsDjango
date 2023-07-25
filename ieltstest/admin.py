a1a3aeb9657682b9a1125970567a7f697a3fe1f9
Kevin Valani <valanikevin@gmail.com>
2023-07-25 01:10:51 -0600

    added admin

--- ieltstest/admin.py
+++ ieltstest/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from ieltstest.models import Test, ListeningSection, ListeningTest, Category
 
-# Register your models here.
+
+class ListeningSectionInline(admin.StackedInline):
+    model = ListeningSection
+    show_change_link = True
+
+
+class ListeningTestInline(admin.StackedInline):
+    model = ListeningTest
+    show_change_link = True
+
+
+class TestAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'id']
+    inlines = [ListeningTestInline]
+
+
+class ListeningTestAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    inlines = [ListeningSectionInline]
+
+
+admin.site.register(ListeningTest, ListeningTestAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(Category)
