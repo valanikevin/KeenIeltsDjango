fc91da692b0065f620f2fed4ff8546cbf9bec71f
Kevin Valani <valanikevin@gmail.com>
2023-07-26 12:32:22 -0600

    modified models

--- ieltstest/admin.py
+++ ieltstest/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from ieltstest.models import Test, ListeningSection, ListeningTest, Category, ListeningQuestionSet
+from ieltstest.models import Test, ListeningSection, ListeningTest, Category
 
 # Inlines
-
-
-class ListeningQuestionSetInline(admin.StackedInline):
-    model = ListeningQuestionSet
-    show_change_link = True
-    extra = 1
 
 
 class ListeningSectionInline(admin.StackedInline):
@@ -32,17 +26,11 @@ class ListeningTestAdmin(admin.ModelAdmin):
     inlines = [ListeningSectionInline]
 
 
-class ListeningQuestionSetAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'title']
-
-
 class ListeningSectionAdmin(admin.ModelAdmin):
     search_fields = ['test']
-    inlines = [ListeningQuestionSetInline,]
 
 
 admin.site.register(ListeningSection, ListeningSectionAdmin)
-admin.site.register(ListeningQuestionSet, ListeningQuestionSetAdmin)
 admin.site.register(ListeningTest, ListeningTestAdmin)
 admin.site.register(Test, TestAdmin)
 admin.site.register(Category)
