893fa9772eb8d2b0b2a797185ebb2b624114ff8e
Kevin Valani <valanikevin@gmail.com>
2023-07-26 19:18:30 -0600

    few more minor model changes

--- ieltstest/admin.py
+++ ieltstest/admin.py
@@ -1,35 +1,54 @@
 from django.contrib import admin
-from ieltstest.models import Test, ListeningSection, ListeningTest
+from ieltstest.models import Test, ListeningSection, ListeningTest, Book
 
 # Inlines
+
+
+class TestInline(admin.StackedInline):
+    model = Test
+    show_change_link = True
+    extra = 1
+    exclude = ['created_at', 'updated_at']
 
 
 class ListeningSectionInline(admin.StackedInline):
     model = ListeningSection
     show_change_link = True
+    extra = 1
 
 
 class ListeningTestInline(admin.StackedInline):
     model = ListeningTest
     show_change_link = True
+    extra = 1
 
 
 # Admins
 
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    inlines = [TestInline,]
+    exclude = ['created_at', 'updated_at']
+
+
 class TestAdmin(admin.ModelAdmin):
     search_fields = ['name', 'id']
     inlines = [ListeningTestInline]
+    exclude = ['created_at', 'updated_at']
 
 
 class ListeningTestAdmin(admin.ModelAdmin):
     search_fields = ['name']
     inlines = [ListeningSectionInline]
+    exclude = ['created_at', 'updated_at']
 
 
 class ListeningSectionAdmin(admin.ModelAdmin):
     search_fields = ['test']
+    exclude = ['created_at', 'updated_at']
 
 
 admin.site.register(ListeningSection, ListeningSectionAdmin)
 admin.site.register(ListeningTest, ListeningTestAdmin)
 admin.site.register(Test, TestAdmin)
+admin.site.register(Book, BookAdmin)
