bb56e3c7d0ec1039af53522b47d88d78928ca8ee
Kevin Valani <valanikevin@gmail.com>
2023-07-28 00:28:41 -0600

    minor changes

--- ieltstest/views.py
+++ ieltstest/views.py
@@ -10,21 +10,15 @@ def ieltstest(request):
     pass
 
 
-def books_with_listening_module():
+def get_books():
     books = Book.objects.order_by('-id')
-    books_id = []
-    for book in books:
-        if book.tests_with_listening_module.exists():
-            books_id .append(book.id)
-
-    books = books.filter(pk__in=books_id)
     return books
 
 
 @api_view(['GET'])
-def test_home(request, slug):
-    books = books_with_listening_module()
-    print(books)
+def module_home(request, slug):
+    books = get_books()
+
     serializer = ListeningTestHomeSerializer(
         {'books': books}, context={'request': request})
     return Response(serializer.data)
