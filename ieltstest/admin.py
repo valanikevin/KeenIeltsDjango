6f0fdb07e165eb39422e23934220b4a77dd86e27
Kevin Valani <valanikevin@gmail.com>
2023-08-07 00:15:42 -0600

    added mode fields for reading module

--- ieltstest/admin.py
+++ ieltstest/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ieltstest.models import Test, ListeningSection, ListeningModule, Book, ListeningAttempt, ListeningSectionQuestionType
+from ieltstest.models import Test, ListeningSection, ListeningModule, Book, ListeningAttempt, ListeningSectionQuestionType, ReadingModule, ReadingSection
 
 # Inlines
 
@@ -23,7 +23,19 @@ class ListeningModuleInline(admin.StackedInline):
     extra = 1
 
 
+class ReadingModuleInline(admin.StackedInline):
+    model = ReadingModule
+    show_change_link = True
+    extra = 1
+
+
+class ReadingSectionInline(admin.StackedInline):
+    model = ReadingSection
+    show_change_link = True
+    extra = 1
+
 # Admins
+
 
 class BookAdmin(admin.ModelAdmin):
     search_fields = ['name']
@@ -33,32 +45,51 @@ class BookAdmin(admin.ModelAdmin):
 
 class TestAdmin(admin.ModelAdmin):
     search_fields = ['name', 'id']
-    inlines = [ListeningModuleInline]
+    inlines = [ListeningModuleInline, ReadingModuleInline]
     exclude = ['created_at', 'updated_at']
 
 
-class ListeningModuleAdmin(admin.ModelAdmin):
+class ModuleAdmin(admin.ModelAdmin):
     search_fields = ['name']
-    inlines = [ListeningSectionInline]
     exclude = ['created_at', 'updated_at']
     list_display = ['name', 'slug']
     readonly_fields = ['total_questions']
 
+    class Meta:
+        abstract = True
 
-class ListeningSectionAdmin(admin.ModelAdmin):
+
+class SectionAdmin(admin.ModelAdmin):
     search_fields = ['test']
     exclude = ['created_at', 'updated_at', ]
     list_display = ['name']
     readonly_fields = ['total_questions']
+
+    class Meta:
+        abstract = True
+
+
+class ListeningModuleAdmin(ModuleAdmin):
+    inlines = [ListeningSectionInline]
+
+
+class ReadingModuleAdmin(ModuleAdmin):
+    inlines = [ReadingSectionInline]
 
 
 class ListeningAttemptAdmin(admin.ModelAdmin):
     list_display = ['user', 'slug', 'status', 'bands']
 
 
-admin.site.register(ListeningSection, ListeningSectionAdmin)
-admin.site.register(ListeningModule, ListeningModuleAdmin)
 admin.site.register(Test, TestAdmin)
 admin.site.register(Book, BookAdmin)
+
+# Listening
 admin.site.register(ListeningAttempt, ListeningAttemptAdmin)
 admin.site.register(ListeningSectionQuestionType)
+admin.site.register(ListeningSection, SectionAdmin)
+admin.site.register(ListeningModule, ListeningModuleAdmin)
+
+# Reading
+admin.site.register(ReadingModule, ReadingModuleAdmin)
+admin.site.register(ReadingSection, SectionAdmin)
