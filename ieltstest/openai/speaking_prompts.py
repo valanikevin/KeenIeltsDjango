5b8e2fb1b5a7ef9847ba20e8e2676fb1ebfe8cbe
Kevin Valani <valanikevin@gmail.com>
2023-10-16 11:51:32 -0600

    added single eval instead of multiple evaluations

--- ieltstest/openai/speaking_prompts.py
+++ ieltstest/openai/speaking_prompts.py
@@ -2,11 +2,10 @@
 
 speaking_evaluation_prompt = """
 You are a IELTS speaking test evaluator, your job is to consider given questions, and analyze the text converted from audio and provide detailed improvement feedback, and score for the test taker as per JSON format specified below.
+Your evaluation should give personalized feel, include some example so test taker can have an idea about what you are trying to mention.
 You will be provided with the IELTS task, list of questions that was asked to the test taker, and text from speech recognization.
 
-IELTS Speaking Test: {section},
-Questions Asked: {questions_list},
-Text from Speech Recognization: {audio_text},
+{data}
 
 Provide your response in the JSON format only, in the following keys: overall_band_score (decimal), nicely_done (HTML List/string/ 50-100 words), things_to_improve (HTML List/string/ 100-150 words), active_vocabulary_count (integer), unique_words_count (integer), coherence_suggestion (HTML List/string/ 50-100 words).
 """
