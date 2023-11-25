4db47f072be12a34402faa33f1082694d52bdcfd
Kevin Valani <valanikevin@gmail.com>
2023-11-25 13:12:59 -0700

    added overall performance chart

--- student/models.py
+++ student/models.py
@@ -39,13 +39,21 @@ class Student(SlugifiedBaseModal):
             if module_attempts.exists():
                 average_bands = module_attempts.aggregate(
                     models.Avg('bands'))['bands__avg']
-                overall_scores[module] = average_bands
+                overall_scores[module] = round_to_half(average_bands)
+
+        # Add overall average bands to overall_scores dict
+        overall_scores['overall'] = round_to_half(sum(
+            overall_scores.values())/len(overall_scores))
 
         return overall_scores
+
+# Function to round off a number to the nearest 0.5
 
     @property
     def fifteen_days_chart(self):
         from ieltstest.variables import get_individual_test_obj_serializer_from_slug, get_module_attempt_from_slug
+        from django.db import models
+        from datetime import datetime, timedelta
 
         modules = ['listening', 'reading', 'writing', 'speaking']
         chart_data = {}
@@ -54,20 +62,63 @@ class Student(SlugifiedBaseModal):
         today = datetime.now().date()
 
         # Generate a list of the last fifteen dates
-        last_fifteen_dates = [today - timedelta(days=i) for i in range(15)]
+        last_fifteen_dates = [today - timedelta(days=i) for i in range(14, -1, -1)]
 
+        # Initialize chart data for each module and each date
         for module in modules:
-            IndividualModuleAttempt, IndividualModuleAttemptSerializer = get_module_attempt_from_slug(
-                module)
-            chart_data[module] = {}
+            IndividualModuleAttempt, IndividualModuleAttemptSerializer = get_module_attempt_from_slug(module)
+            chart_data[module] = {date.strftime("%B %-d, %Y"): {'average_bands': None, 'attempt_count': 0} for date in last_fifteen_dates}
+
             for date in last_fifteen_dates:
                 # Find average score of all attempts
                 module_attempts = IndividualModuleAttempt.objects.filter(
                     user=self.user, status="Evaluated", created_at__date=date)
                 if module_attempts.exists():
-                    average_bands = module_attempts.aggregate(
-                        models.Avg('bands'))['bands__avg']
-                    chart_data[module][date.strftime(
-                        "%B %-d, %Y")] = average_bands
+                    average_bands = module_attempts.aggregate(models.Avg('bands'))['bands__avg']
+                    chart_data[module][date.strftime("%B %-d, %Y")]['average_bands'] = round_to_half(average_bands)
+                    chart_data[module][date.strftime("%B %-d, %Y")]['attempt_count'] = module_attempts.count()
 
-        return chart_data
+        # Add Overall Average Bands for the last fifteen days from chart_data of each module
+        chart_data['overall'] = {}
+        for date in last_fifteen_dates:
+            overall_average_bands = 0
+            total_attempts = 0
+            module_count = 0
+            for module in modules:
+                module_data = chart_data[module][date.strftime("%B %-d, %Y")]
+                if module_data['average_bands'] is not None:
+                    overall_average_bands += module_data['average_bands']
+                    total_attempts += module_data['attempt_count']
+                    module_count += 1
+            if module_count > 0:
+                chart_data['overall'][date.strftime("%B %-d, %Y")] = {'average_bands': round_to_half(overall_average_bands/module_count), 'attempt_count': total_attempts}
+            else:
+                chart_data['overall'][date.strftime("%B %-d, %Y")] = {'average_bands': None, 'attempt_count': 0}
+
+        # Prepare chart format with date labels and attempt counts
+        chart_format = {
+            module: {
+                'average_bands': [data['average_bands'] for data in chart_data[module].values()],
+                'attempt_count': [data['attempt_count'] for data in chart_data[module].values()]
+            } for module in modules
+        }
+        chart_format['overall'] = {
+            'average_bands': [data['average_bands'] for data in chart_data['overall'].values()],
+            'attempt_count': [data['attempt_count'] for data in chart_data['overall'].values()]
+        }
+        chart_format['dates'] = [date.strftime("%B %-d, %Y") for date in last_fifteen_dates]
+
+        return chart_format
+
+
+
+def round_to_half(number):
+    rounded = round(number * 2) / 2
+
+    # number should not be less than 1 and more than 9
+    if rounded < 1:
+        return 1
+    elif rounded > 9:
+        return 9
+    else:
+        return rounded
