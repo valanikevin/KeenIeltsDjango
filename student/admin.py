20f66ba5308fc50faa4836af0b48033858ed9e85
Kevin Valani <valanikevin@gmail.com>
2023-11-27 11:42:21 -0700

    minor changes

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from student.models import Student, OverallPerformanceFeedback
 
-# Register your models here.
+
+class OverallPerformanceFeedbackInline(admin.StackedInline):
+    model = OverallPerformanceFeedback
+    show_change_link = True
+    extra = 1
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'institute', 'type', 'bandsTarget')
+    inlines = [OverallPerformanceFeedbackInline]
+
+
+admin.site.register(Student, StudentAdmin)
