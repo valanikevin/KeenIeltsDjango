02b56574bee029bc14a064f8a90f8f6724d5a207
Kevin Valani <valanikevin@gmail.com>
2023-11-27 11:42:21 -0700

    minor changes

--- student/views.py
+++ student/views.py
@@ -9,8 +9,18 @@ from rest_framework.permissions import IsAuthenticated
 def overall_performance(request):
     student = request.user.student
 
+    total_tests = student.average_score["overall"]["total_attempts"]
+    if total_tests == 0:
+        return Response({
+            'overall_feedback_date': None,
+            'overall_feedback': None,
+            'average_score': student.average_score,
+            'fifteen_days_chart': None,
+            'recent_tests': None,
+        }, status=200)
+
     return Response({
-        'overall_feedback_date': student.overallperformancefeedback.updated_at.strftime("%B %d, %Y"),
+        'overall_feedback_date': student.overallperformancefeedback.updated_at.strftime("%B %d, %Y") if student.overallperformancefeedback else None,
         'overall_feedback': student.overall_feedback,
         'average_score': student.average_score,
         'fifteen_days_chart': student.fifteen_days_chart,
