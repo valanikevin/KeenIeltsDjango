1b18141ce8372437d87b656072deb46dc3cd739c
Kevin Valani <valanikevin@gmail.com>
2023-12-01 00:48:57 -0700

    added itinerary code

--- KeenIeltsDjango/urls.py
+++ KeenIeltsDjango/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from KeenIeltsDjango import wanderwave
 
 urlpatterns = [
+    path('wanderwave/', wanderwave.generate_itinerary, name='wanderwave'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
