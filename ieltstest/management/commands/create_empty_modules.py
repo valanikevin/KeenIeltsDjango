5c0da9330ba3eff1e95d7d1c1500983d91c4d100
Kevin Valani <valanikevin@gmail.com>
2023-12-11 23:51:53 -0700

    Add functionality to create empty modules for a book

--- ieltstest/management/commands/create_empty_modules.py
+++ ieltstest/management/commands/create_empty_modules.py
@@ -12,7 +12,13 @@ class Command(BaseCommand):
             print(f'Book: {book.name} | ID: {book.id}')
             create_test = input('Create empty modules for this book? (y/n): ')
             if create_test.lower() == 'y':
-                book.create_empty_tests()
+                no_of_practice_tests = int(
+                    input('Enter number of practice tests: '))
+                module_type = input(
+                    'Enter module type (1. academic, 2.general): ')
+                module_type = 'academic' if module_type == '1' else 'general'
+                book.create_empty_tests(
+                    no_of_practice_test=no_of_practice_tests, module_type=module_type)
                 print('Done!')
             else:
                 print('Skipped!')
