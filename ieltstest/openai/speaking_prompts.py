c49d93d7e6ea0c1e0b53ecea1b173f89ea3ed593
Kevin Valani <valanikevin@gmail.com>
2024-01-10 22:53:53 -0700

    Add additional human messages to the chat model

--- ieltstest/openai/speaking_prompts.py
+++ ieltstest/openai/speaking_prompts.py
@@ -1,9 +1,8 @@
 # Speaking Prompt Template
 
 speaking_evaluation_prompt = """
-As an exacting evaluator for the IELTS speaking test, your role involves a detailed and strict assessment of a student's spoken responses. It's imperative that you analyze the transcriptions of the test taker's answers thoroughly, bearing in mind the specifics of the IELTS speaking task and the questions posed. Your feedback should be precise, direct, and utilize quotes from the student's responses to highlight specific shortcomings or areas of improvement. The goal is to provide feedback that is not only clear and constructive but also actionable for the test taker.
+As an strict evaluator for the IELTS speaking test, your role involves a detailed and strict assessment of a student's spoken responses. It's imperative that you analyze the transcriptions of the test taker's answers thoroughly, bearing in mind the specifics of the IELTS speaking task and the questions posed. Your feedback should be precise, direct, and utilize quotes from the student's responses to highlight specific shortcomings or areas of improvement. The goal is to provide feedback that is not only clear and constructive but also actionable for the test taker.
 
-You should not hesitate to assign a score as low as 5 bands if the responses are not satisfactory or do not meet the IELTS criteria. Your evaluation, including personalized feedback and a score, should be formatted in JSON, ensuring a comprehensive and detailed analysis based on the IELTS standards.
 To conduct this evaluation, you will need:
 
 1. Details of the specific IELTS speaking task.
@@ -11,6 +10,12 @@ To conduct this evaluation, you will need:
 3. The transcribed text of the test taker's responses.
 
 {data}
+"""
 
+speaking_evaluation_prompt1 = """
+You should not hesitate to assign a score as low as 5 bands if the responses are not satisfactory or do not meet the IELTS criteria. If the respons is satisfactory, you must give deserving bands to the student, even it is bands 8. Your evaluation, including personalized feedback and a score, should be formatted in JSON, ensuring a comprehensive and detailed analysis based on the IELTS standards.
+"""
+
+speaking_evaluation_prompt2 = """
 Provide your response in the JSON format only, in the following keys: overall_band_score (decimal),fluency_and_coherence_bands (decimal), grammatical_range_and_accuracy_bands (decimal) , lexical_resource_bands (decimal), pronunciation_bands (decimal), overall_personalized_feedback_suggestions (string/100-200 words), word_choice_suggestions (Python List 3/4 points/150-250 words).
 """
